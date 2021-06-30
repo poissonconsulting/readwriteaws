b5b40bbdd6d8675ebc2c3a95a997c66d8535a5c0
Ayla Pearson <>
2021-06-30 15:39:51 -0700

    removing random message in read files

--- R/rwa-read-files.R
+++ R/rwa-read-files.R
@@ -5,6 +5,8 @@
 #'
 #' @export
 #'
+#'
+### change back to list
 rwa_read_files <- function(bucket_name,
                            year = "",
                            module_name = "",
@@ -20,19 +22,5 @@ rwa_read_files <- function(bucket_name,
 
   key_names <- lapply(file_list$Contents, function(x) {x$Key})
   key_names <- unlist(key_names)
-  number_of_files <- length(key_names)
-
-  usethis::ui_info(glue::glue("There are {number_of_files} files in {bucket_name}. If
-                              over 1000 then you need to narrow the query."))
-
-  ### think this will need to return a list where the first item is total number of
-  ### files
-  ### then the "wrapper" functions can test against it and resend a request without
-  ### the user knowing - so when get_tracks  is called if the base call is over
-  ### 1000 then it will ignore a bunch of other files until the request works
-  ### the user won't know on the front end the search was narrowed just in case
-
   key_names
 }
-
-
