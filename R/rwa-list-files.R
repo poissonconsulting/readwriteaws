17bc64601d0c2bfa631265ab0c6234748023358c
Ayla Pearson <>
2021-07-01 15:20:26 -0700

    adding regex functionality to list-files function

--- R/rwa-list-files.R
+++ R/rwa-list-files.R
@@ -6,21 +6,21 @@
 #' @export
 #'
 #'
-### change back to list
-rwa_list_files <- function(bucket_name,
-                           year = "",
-                           module_name = "",
-                           file_type = "") {
-  chk::chk_string(bucket_name)
-  chk::chk_string(year)
-  chk::chk_string(module_name)
 
-  chk::chk_string(file_type)
+rwa_list_files <- function(bucket_name,
+                           ### it can also be NULL not sure whats a better default value
+                           ### how does user know if they should increase? print total number returned?
+                           max_request_size = 1000L,
+                           pattern = ".*") {
+  chk::chk_string(bucket_name)
+  chk::chk_integer(max_request_size)
+  chk::chk_string(pattern)
 
   s3 <- paws::s3()
-  file_list <- s3$list_objects_v2(Bucket = bucket_name)
+  key_list <- s3$list_objects_v2(Bucket = bucket_name, MaxKeys = max_request_size)
 
-  key_names <- lapply(file_list$Contents, function(x) {x$Key})
+  key_names <- lapply(key_list$Contents, function(x) {x$Key})
   key_names <- unlist(key_names)
-  key_names
+
+  grep(pattern, key_names, value = TRUE)
 }
