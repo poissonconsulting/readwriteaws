afa5d65d7f29dc3795349a304be1d3b9d48658d5
Ayla Pearson <>
2021-07-05 15:29:06 -0700

    docs

--- R/rwa-list-files.R
+++ R/rwa-list-files.R
@@ -2,9 +2,20 @@
 #'
 #' Lists all files from s3 bucket
 #' @inheritParams params
+#' @details The AWS API only allows a max of 1000 objects to return per request.
+#' The `max_request_size` argument is for creating multiple requests to the API
+#' so you are able to get all the files if there are more then 1000 objects
+#' in your bucket. If `max_request_size` is over 1000 it will make multiple
+#' request to AWS until the `max_request_size` is reached.
+#'
+#' The `pattern` argument is a regex that is applied after all the files are
+#' obtained from AWS. If this argument is given you may get less
+#' then the value given for `max_request_size` as it will only return the
+#' objects that match the pattern.
+#'
 #' @examples
 #' \dontrun{
-#' # List all files in the bucket
+#' # List all files in the bucket up to a max of 1000
 #' rwa_list_files(bucket_name = "my-project-bucket")
 #'
 #' # Add regex to only list pdfs from June 30th
@@ -14,11 +25,13 @@
 #' # Add regex to only list files from 2021
 #' rwa_list_files(bucket_name = "my-project-bucket",
 #'                pattern = "2021")
+#'
+#' # List all files in the bucket up to a max of 2000
+#' rwa_list_files(bucket_name = "my-project-bucket",
+#'                max_request_size = 2000)
 #' }
 #' @export
 rwa_list_files <- function(bucket_name,
-                           ### it can also be NULL not sure whats a better default value
-                           ### how does user know if they should increase? print total number returned?
                            max_request_size = 1000,
                            pattern = ".*") {
   chk::chk_string(bucket_name)
@@ -30,7 +43,10 @@ rwa_list_files <- function(bucket_name,
   all_keys <- c("")
   s3 <- paws::s3()
   while (max_request_size > 0) {
-      key_list <- s3$list_objects_v2(Bucket =  bucket_name, MaxKeys = max_request_size, StartAfter = start_after)
+      key_list <- s3$list_objects_v2(Bucket =  bucket_name,
+                                     MaxKeys = max_request_size,
+                                     StartAfter = start_after
+                                    )
       key_names <- vapply(key_list$Contents, function(x) {x$Key}, "")
       all_keys <- c(all_keys, key_names)
       n <- length(key_names)
