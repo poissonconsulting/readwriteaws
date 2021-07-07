e9fd847a6f82e7ea58d99e547b94a626a95734db
Ayla Pearson <ayla@poissonconsulting.ca>
2021-07-07 11:17:44 -0700

    updating parameter names and descriptions

--- R/params.R
+++ R/params.R
@@ -9,15 +9,16 @@
 #'
 #' @param pattern A character string containing a regular expression to be matched in the given character vector
 #' @param max_request_size An integer indicating the maximum request size sent to AWS API
-#' @param silent A flag to turn on and off messages about bucket object numbers
+#' @param silent A logical flag to turn on and off messages about number of files returned
 #'
-#' @param year Regex filter param
-#' @param month Regex filter param - format MM
-#' @param day Regex filter param - format DD
-#' @param data_type Regex filter param
-#' @param file_extension Regex filter param
-#' @param file_name Regex filter param
+#' @param year A numeric value for what years you want returned. Format YYYY
+#' @param month A numeric value for what month you want returned. Format MM
+#' @param day A numeric value for what day you want returned. Format DD
+#' @param data_type A character string for which data you want to return. Options currently include punch-data, tracks, logger, image and pdf.
 #'
-#' @param file_names A vector containing a list of files from the object names in AWS
+#' @param file_extension A character string for which file extension you want without the period
+#' @param file_name A character string for which file names you want
+#'
+#' @param file_list A vector containing a list of files from the object names in AWS
 #' @param directory A string that names the folder you want to save the objects into.
 NULL
--- R/rwa-write-files.R
+++ R/rwa-write-files.R
@@ -5,14 +5,14 @@
 #'
 #' @export
 #'
-rwa_write_files <- function(file_names, directory, bucket_name) {
-  chk::chk_vector(file_names)
+rwa_write_files <- function(file_list, directory, bucket_name) {
+  chk::chk_vector(file_list)
   chk::chk_string(directory)
 
-  pb <- progress::progress_bar$new(total = length(file_names))
+  pb <- progress::progress_bar$new(total = length(file_list))
 
   pb$tick(0)
-  for (file in file_names) {
+  for (file in file_list) {
     pb$tick()
     save_location <- paste0(directory, "/", file)
     aws.s3::save_object(
