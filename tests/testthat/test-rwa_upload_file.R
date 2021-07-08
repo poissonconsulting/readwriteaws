e0e9cb84ab80d6dede6856832cdf344521428837
Ayla Pearson <ayla@poissonconsulting.ca>
2021-07-08 13:51:22 -0700

    switching tests to be with withr pkg then local files

--- tests/testthat/test-rwa_upload_file.R
+++ tests/testthat/test-rwa_upload_file.R
@@ -1,47 +1,99 @@
 
 bucket_name <- "readwriteaws-test-poissonconsulting"
 
-file_01 <- "tests/shiny-upload/upload_test/demo-1.pdf"
-file_02 <- "tests/shiny-upload/upload_test/demo-2.pdf"
-directory <- "inst"
-key_path_01 <- paste0(directory, "/", file_01)
-key_path_02 <- paste0(directory, "/", file_02)
-multi_files <- c(file_01, file_02)
-
 s3_bucket <- paws::s3()
 
-test_that("test files are not in bucket", {
+test_that("put single file in a bucket", {
+  # set up
+  x <- c(1, 2, 3)
+  temp_folder <- withr::local_tempdir()
+  saveRDS(data.frame(x = 1), file.path(temp_folder, "test.rds"))
+  file_name <- file.path(temp_folder, "test.rds")
+  # check file not present
   expect_error(
     s3_bucket$get_object(
       Bucket = bucket_name,
-      Key = key_path_01
+      Key = file_name
     ),
     regexp = "HTTP 404"
   )
-
+  # upload files to AWS
+  rwa_upload_files(
+    file_list = file_name,
+    directory = "",
+    bucket_name = bucket_name,
+    bucket_path = ""
+  )
+  # test the file is uploaded
+  expect_type(
+    s3_bucket$get_object(
+      Bucket = bucket_name,
+      Key = file_name
+    ),
+    "list"
+  )
+  # delete object
+  expect_type(
+    s3_bucket$delete_object(
+      Bucket = bucket_name,
+      Key = file_name
+    ),
+    "list"
+  )
+  # check file not present
   expect_error(
     s3_bucket$get_object(
       Bucket = bucket_name,
-      Key = key_path_02
+      Key = file_name
     ),
     regexp = "HTTP 404"
   )
-
 })
 
-test_that("put files in bucket and confirm present", {
+test_that("put multiple files in a bucket", {
+  # set up
+  x <- c(1, 2, 3)
+  temp_folder <- withr::local_tempdir()
+  saveRDS(data.frame(x = 1), file.path(temp_folder, "test_01.rds"))
+  file_name_01 <- file.path(temp_folder, "test_01.rds")
 
+  y <- c(1, 2, 3)
+  temp_folder <- withr::local_tempdir()
+  saveRDS(data.frame(x = 1), file.path(temp_folder, "test_02.rds"))
+  file_name_02 <- file.path(temp_folder, "test_02.rds")
+
+  multi_files <- c(file_name_01, file_name_02)
+
+  # check file not present
+  expect_error(
+    s3_bucket$get_object(
+      Bucket = bucket_name,
+      Key = file_name_01
+    ),
+    regexp = "HTTP 404"
+  )
+
+  expect_error(
+    s3_bucket$get_object(
+      Bucket = bucket_name,
+      Key = file_name_02
+    ),
+    regexp = "HTTP 404"
+  )
+
+  # upload files to AWS
   rwa_upload_files(
     file_list = multi_files,
-    directory = directory,
+    directory = "",
     bucket_name = bucket_name,
     bucket_path = ""
   )
 
+  # test the file is uploaded
   expect_type(
     s3_bucket$get_object(
       Bucket = bucket_name,
-      Key = key_path_01
+      Key = file_name_01
     ),
     "list"
   )
@@ -49,17 +101,16 @@ test_that("put files in bucket and confirm present", {
   expect_type(
     s3_bucket$get_object(
       Bucket = bucket_name,
-      Key = key_path_02
+      Key = file_name_02
     ),
     "list"
   )
-})
 
-test_that("delete files - ie clean up", {
+  # delete object
   expect_type(
     s3_bucket$delete_object(
       Bucket = bucket_name,
-      Key = key_path_01
+      Key = file_name_01
     ),
     "list"
   )
@@ -67,20 +118,74 @@ test_that("delete files - ie clean up", {
   expect_type(
     s3_bucket$delete_object(
       Bucket = bucket_name,
-      Key = key_path_02
+      Key = file_name_02
     ),
     "list"
   )
 
+  # check file not present
+  expect_error(
+    s3_bucket$get_object(
+      Bucket = bucket_name,
+      Key = file_name_01
+    ),
+    regexp = "HTTP 404"
+  )
+
+  expect_error(
+    s3_bucket$get_object(
+      Bucket = bucket_name,
+      Key = file_name_02
+    ),
+    regexp = "HTTP 404"
+  )
 })
 
 #### Test inputs ####
 
+test_that("function should error if list of files instead of vector given", {
+  files_in_list <- list(1, 2)
 
+  expect_error(
+    rwa_upload_files(
+      file_list = files_in_list,
+      directory = "R",
+      bucket_name = bucket_name
+    ),
+    regexp = "must be character"
+  )
+})
 
+test_that("function should error when directory is not a string", {
+  expect_error(
+    rwa_upload_files(
+      file_list = "functions.R",
+      directory = 01,
+      bucket_name = bucket_name
+    ),
+    regexp = "must be a string"
+  )
+})
 
+test_that("function should error when bucket is not a string", {
+  expect_error(
+    rwa_upload_files(
+      file_list = "functions.R",
+      directory = "R",
+      bucket_name = 01
+    ),
+    regexp = "must be a string"
+  )
+})
 
-
-
-
-
+test_that("function should error when bucket_path is not a string", {
+  expect_error(
+    rwa_upload_files(
+      file_list = "functions.R",
+      directory = "R",
+      bucket_name = bucket_name,
+      bucket_path = 01
+    ),
+    regexp = "must be a string"
+  )
+})
