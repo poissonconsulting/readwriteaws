f846ef2f7f13c9d823e20727964d866c0a272fb5
Ayla Pearson <ayla@poissonconsulting.ca>
2021-07-08 13:51:54 -0700

    clean up

--- R/rwa_upload_file.R
+++ R/rwa_upload_file.R
@@ -13,25 +13,19 @@ rwa_upload_files <- function(file_list,
   chk::chk_character(file_list)
   # chk::chk_dir(directory)
   ### should it be chk_dir
-  # chk::chk_character(directory)
+  chk::chk_string(directory)
   chk::chk_string(bucket_name)
   chk::chk_string(bucket_path)
 
   for (file in file_list) {
 
-    local_path <- paste0(directory, "/", file)
-
-    print(local_path)
-    print("--------")
-
-    key_path <- paste0(bucket_path, "/", local_path)
-
-    print(key_path)
+    local_path <- file.path(directory, file)
+    key_path <- file.path(bucket_path, local_path)
 
     s3_bucket <- paws::s3()
     s3_bucket$put_object(Body = local_path, ## this is the local file path
                          Bucket = bucket_name,
-                         ## where it goes in AWS
-                         Key = key_path)
+                         Key = key_path) ## where it goes in AWS
   }
 }
+
